ba94b5639a86be03ec7c29b22ea14b4c1346fdb1
Natalie Akam <natalie.akam@gmail.com>
2015-12-31 10:12:32 +0000

    Adds factory

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const app = angular.module('news', []);
 
-app.controller('MainCtrl', [function() {
+app.factory('postFactory', [() => {
+  const postList = {
+    posts: []
+  };
+  return postList;
+}]);
+
+app.controller('MainCtrl', ['postFactory', function(postFactory) {
   const self = this;
-  self.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+
+  self.posts = postFactory.posts;
 
   self.addPost = () => {
     if (!self.title || self.title === '') {return}
