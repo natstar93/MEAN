58d623521e1ef5b1621b0ee9adb91458b124b5fa
Natalie Akam <natalie.akam@gmail.com>
2016-01-03 18:04:10 +0000

    Adds upvote route and schema method

--- news/routes/index.js
+++ news/routes/index.js
@@ -4,26 +4,32 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-
-router.param('post', (req, res, next, id) => {
-  console.log('hi');
+router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
-  query.exec((err, post) => {
-    if(err) { return next(err); }
-    if(!post) { return next(new Error('can\'t find post')); }
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
 
     req.post = post;
     return next();
   });
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/posts/:post', (req, res) => {
+router.get('/posts', (req, res, next) => {
+  Post.find((err, posts) => {
+    if(err) { return next(err); }
     res.json(posts);
+  });
+});
+
+router.get('/posts/:post', (req, res) => {
+  res.json(req.post);
 });
 
 router.post('/posts', (req, res, next) => {
@@ -31,6 +37,14 @@ router.post('/posts', (req, res, next) => {
 
   post.save((err, post) => {
     if(err) { return next(err); }
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/upvote', (req, res, next) => {
+  req.post.upvote((err, post) => {
+    if (err) { return next(err); }
+
     res.json(post);
   });
 });
